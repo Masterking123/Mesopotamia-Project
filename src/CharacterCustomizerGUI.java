2f22a46511e0dca6d11e3eaefa5eef2d7bfb2308
KCheng259 <kaidenc259@gmail.com>
2023-11-20 08:08:21 -0500

    rewrite gui

--- src/CharacterCustomizerGUI.java
+++ src/CharacterCustomizerGUI.java
@@ -1,18 +1,36 @@
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
 
-public class CharacterCustomizerGUI {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+public class CharacterCustomizerGUI extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainGameGUI frame = new MainGameGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	public CharacterCustomizerGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
 
 }
