96b962ad3c7351a831340c1f04addd0d0921dc2b
SunrajS <ssihra.singh@gmail.com>
2023-11-21 20:53:54 -0500

    finished main menu

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -1,6 +1,13 @@
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -35,6 +42,34 @@ public class MainMenuGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JLabel lblNewLabel_1 = new JLabel("Mesopotamia: Dawn of Civilization");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(82, 50, 289, 33);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Start");
+		btnNewButton.setBounds(150, 121, 133, 38);
+		contentPane.add(btnNewButton);
+		
+		JButton btnHelp = new JButton("Help");
+		btnHelp.setBounds(150, 169, 133, 38);
+		contentPane.add(btnHelp);
+		
+		JButton btnQuit = new JButton("Quit");
+		btnQuit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		btnQuit.setBounds(150, 215, 133, 38);
+		contentPane.add(btnQuit);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		Image img = new ImageIcon(this.getClass().getResource("bonzeagetest2.jpg")).getImage();
+		lblNewLabel.setIcon(new ImageIcon(img));
+		lblNewLabel.setBounds(10, 10, 416, 243);
+		contentPane.add(lblNewLabel);
 	}
 
 }
