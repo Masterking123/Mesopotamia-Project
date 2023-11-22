210e76052c91125b45e7288e82ad0d041dcac5af
SavneetB <Savneetk.bal@gmail.com>
2023-11-22 07:56:48 -0500

    Added variables for military/farmer/miner/researcher

--- src/MainGameGUI.java
+++ src/MainGameGUI.java
@@ -8,12 +8,19 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JToolBar;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 
 public class MainGameGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	public double military; 
+	public double farmer; 
+	public double miner; 
+	public double research; 
 
 	/**
 	 * Launch the application.
@@ -39,7 +46,13 @@ public class MainGameGUI extends JFrame {
 	public MainGameGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1650, 1080);
-		contentPane = new JPanel();
+		contentPane = new JPanel() {  
+			public void paintComponent(Graphics g) {  
+          Image img = Toolkit.getDefaultToolkit().getImage(  
+          MainGameGUI.class.getResource("/images/gameBackgroundImage.jpg"));  
+          g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);  
+              }  
+          };  
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setForeground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
