1d532385f5e3068ae9655df52bc9f71b196beae9
ndhaliwal59 <59dhaliwal59@gmail.com>
2023-11-24 20:44:27 -0500

    researchGUI done

---
+++ src/research.java
@@ -0,0 +1,419 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class research extends MainGameGUI {
+
+	private JPanel contentPane;
+	private JLabel ptsLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					research frame = new research();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	void popUp() {
+		JOptionPane.showMessageDialog(this, "Not enough Points");
+	}
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public research() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 750, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		ptsLabel = new JLabel("Points: " + research);
+		ptsLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		ptsLabel.setBounds(16, 16, 204, 28);
+		contentPane.add(ptsLabel);
+		
+		JLabel resaerchTitle = new JLabel("Research");
+		resaerchTitle.setFont(new Font("Lucida Grande", Font.PLAIN, 38));
+		resaerchTitle.setBounds(281, 6, 165, 38);
+		contentPane.add(resaerchTitle);
+		
+		JLabel farmLabel = new JLabel("Farming");
+		farmLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		farmLabel.setBounds(99, 74, 81, 22);
+		contentPane.add(farmLabel);
+		
+		JLabel salesLabel = new JLabel("Sales");
+		salesLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		salesLabel.setBounds(611, 74, 61, 22);
+		contentPane.add(salesLabel);
+		
+		JLabel mineLabel = new JLabel("Mining");
+		mineLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		mineLabel.setBounds(268, 74, 61, 22);
+		contentPane.add(mineLabel);
+		
+		JLabel milataryLabel = new JLabel("Military");
+		milataryLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		milataryLabel.setBounds(442, 74, 81, 22);
+		contentPane.add(milataryLabel);
+		
+		JButton waterFilterButton = new JButton("Water Filter");
+		waterFilterButton.setBounds(73, 153, 117, 45);
+		contentPane.add(waterFilterButton);
+		
+		JButton sToolsButton = new JButton("Stone Tools");
+		sToolsButton.setBounds(73, 222, 117, 45);
+		contentPane.add(sToolsButton);
+		
+		JButton sSeedsButton = new JButton("Super Seeds");
+		sSeedsButton.setBounds(73, 292, 117, 45);
+		contentPane.add(sSeedsButton);
+		
+		JButton exFarmButton = new JButton("Expanded Farm");
+		exFarmButton.setBounds(73, 364, 117, 45);
+		contentPane.add(exFarmButton);
+		
+		
+		JLabel pts3Label = new JLabel("3 Points");
+		pts3Label.setBounds(6, 166, 61, 16);
+		contentPane.add(pts3Label);
+		
+		JLabel pts6Label = new JLabel("6 Points");
+		pts6Label.setBounds(6, 235, 61, 16);
+		contentPane.add(pts6Label);
+		
+		JLabel pts9Label = new JLabel("9 Points");
+		pts9Label.setBounds(6, 305, 61, 16);
+		contentPane.add(pts9Label);
+		
+		JLabel pts12Label = new JLabel("12 Points");
+		pts12Label.setBounds(6, 377, 61, 16);
+		contentPane.add(pts12Label);
+		
+		JButton exitResButton = new JButton("Exit"); //exit button
+		exitResButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		exitResButton.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		exitResButton.setBounds(667, 6, 77, 38);
+		contentPane.add(exitResButton);
+		
+		JButton stoneToolsButton = new JButton("Stone Tools");
+		stoneToolsButton.setToolTipText("10% more food");
+		stoneToolsButton.setBounds(235, 153, 117, 45);
+		contentPane.add(stoneToolsButton);
+		
+		JButton eSafetyButton = new JButton("Extra Safety");
+		eSafetyButton.setEnabled(false);
+		eSafetyButton.setBounds(235, 222, 117, 45);
+		contentPane.add(eSafetyButton);
+		
+		JButton ironToolsButton = new JButton("Iron Tools");
+		ironToolsButton.setEnabled(false);
+		ironToolsButton.setBounds(235, 292, 117, 45);
+		contentPane.add(ironToolsButton);
+		
+		JButton goldButton = new JButton("Gold Mine");
+		goldButton.setEnabled(false);
+		goldButton.setBounds(235, 364, 117, 45);
+		contentPane.add(goldButton);
+		
+		JButton lArmButton = new JButton("Light Armour");
+		lArmButton.setBounds(418, 153, 117, 45);
+		contentPane.add(lArmButton);
+		
+		JButton hArmourButton = new JButton("Heavy Armour");
+		hArmourButton.setEnabled(false);
+		hArmourButton.setBounds(418, 222, 117, 45);
+		contentPane.add(hArmourButton);
+		
+		JButton blackButton = new JButton("Master Blacksmith");
+		blackButton.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		blackButton.setEnabled(false);
+		blackButton.setBounds(418, 292, 117, 45);
+		contentPane.add(blackButton);
+		
+		JButton spyButton = new JButton("Spies");
+		spyButton.setEnabled(false);
+		spyButton.setBounds(418, 364, 117, 45);
+		contentPane.add(spyButton);
+		
+		JButton sale1Button = new JButton("Makret Sale 1");
+		sale1Button.setBounds(579, 153, 117, 45);
+		contentPane.add(sale1Button);
+		
+		JButton sale2Button = new JButton("Market Sale 2");
+		sale2Button.setEnabled(false);
+		sale2Button.setBounds(579, 222, 117, 45);
+		contentPane.add(sale2Button);
+		
+		JButton sale3Button = new JButton("Market Sale 3");
+		sale3Button.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		sale3Button.setEnabled(false);
+		sale3Button.setBounds(579, 292, 117, 45);
+		contentPane.add(sale3Button);
+		
+		JButton sale4Button = new JButton("Market Sale 4");
+		sale4Button.setEnabled(false);
+		sale4Button.setBounds(579, 364, 117, 45);
+		contentPane.add(sale4Button);
+		
+		//------------------Farming section------------------//
+		waterFilterButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 3) {
+					research = research -3;
+					waterFilterButton.setEnabled(false);
+					sToolsButton.setEnabled(true);
+					ptsLabel.setText("Points: " + research);
+					//the boost
+					waterFilterButton.setToolTipText("10% more food");
+				}
+				else {
+					popUp();
+				}
+			}});
+		sToolsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 6) {
+					research = research - 6;
+					sToolsButton.setEnabled(false);
+					sSeedsButton.setEnabled(true);
+					//ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		sSeedsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 6) {
+					research = research - 6;
+					sSeedsButton.setEnabled(false);
+					exFarmButton.setEnabled(true);
+					//ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		exFarmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 12) {
+					research = research - 12;
+					exFarmButton.setEnabled(false);
+					//ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		//------------------Mining section------------------//
+		stoneToolsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 3) {
+					research = research -3;
+					stoneToolsButton.setEnabled(false);
+					eSafetyButton.setEnabled(true);
+					ptsLabel.setText("Points: " + research);
+					//the boost
+					stoneToolsButton.setToolTipText("+10 resources per day");
+				}
+				else {
+					popUp();
+				}
+			}});
+		eSafetyButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 6) {
+					research = research - 6;
+					eSafetyButton.setEnabled(false);
+					ironToolsButton.setEnabled(true);
+					ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		ironToolsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 6) {
+					research = research - 6;
+					ironToolsButton.setEnabled(false);
+					goldButton.setEnabled(true);
+					ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		goldButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(research >= 12) {
+					research = research - 12;
+					goldButton.setEnabled(false);
+					ptsLabel.setText("Points: " + research);
+					//the boost
+				}
+				else {
+					popUp();
+				}
+			}});
+		
+		//-----------------Military section------------------//
+		lArmButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 3) {
+							research = research -3;
+							lArmButton.setEnabled(false);
+							hArmourButton.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+							lArmButton.setToolTipText("+10 resources per day");
+						}
+						else {
+							popUp();
+						}
+					}});
+		hArmourButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 6) {
+							research = research - 6;
+							hArmourButton.setEnabled(false);
+							blackButton.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		blackButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 6) {
+							research = research - 6;
+							blackButton.setEnabled(false);
+							spyButton.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		spyButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 12) {
+							research = research - 12;
+							spyButton.setEnabled(false);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		
+		//-----------------Sales section------------------//
+		sale1Button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 3) {
+							research = research -3;
+							sale1Button.setEnabled(false);
+							sale2Button.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+							sale1Button.setToolTipText("+10 resources per day");
+						}
+						else {
+							popUp();
+						}
+					}});
+		sale2Button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 6) {
+							research = research - 6;
+							sale2Button.setEnabled(false);
+							sale3Button.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		sale3Button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 6) {
+							research = research - 6;
+							sale3Button.setEnabled(false);
+							sale4Button.setEnabled(true);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		sale4Button.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(research >= 12) {
+							research = research - 12;
+							sale4Button.setEnabled(false);
+							ptsLabel.setText("Points: " + research);
+							//the boost
+						}
+						else {
+							popUp();
+						}
+					}});
+		
+		sToolsButton.setEnabled(false);
+		sSeedsButton.setEnabled(false);
+		exFarmButton.setEnabled(false);
+		
+		eSafetyButton.setEnabled(false);
+		ironToolsButton.setEnabled(false);
+		goldButton.setEnabled(false);
+		
+		hArmourButton.setEnabled(false);
+		blackButton.setEnabled(false);
+		spyButton.setEnabled(false);
+		
+		sale2Button.setEnabled(false);
+		sale3Button.setEnabled(false);
+		sale4Button.setEnabled(false);
+		
+	}
+}
