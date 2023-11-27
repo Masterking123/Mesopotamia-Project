6b7a4e7e9c629eb5acefd8a5e23b46898317fdf4
KCheng259 <kaidenc259@gmail.com>
2023-11-27 10:42:06 -0500

    created player object

---
+++ src/PlayerObject.java
@@ -0,0 +1,14 @@
+
+public class PlayerObject {
+	public static String playerName;
+	public static String playerVillageName;
+	public static String rivalVillageName;
+	public static int food;
+	public static int foodBoost;
+	public static int miningAndWood;
+	public static int miningAndWoodBoost; 
+	public static int people;
+	public static int military;
+	public static int militaryBoost;
+	public static int reputation;	
+}
