850b64f023a736a7104289dfb37909dcd374405f
ndhaliwal59 <59dhaliwal59@gmail.com>
2023-11-28 08:23:31 -0500

    new boost variables

--- src/PlayerObject.java
+++ src/PlayerObject.java
@@ -1,14 +1,23 @@
 
 public class PlayerObject {
+	
+	// Percentage is stored as a whole number, convert to decimal when calculating
 	public static String playerName;
 	public static String playerVillageName;
 	public static String rivalVillageName;
-	public static int food;
-	public static int foodBoost;
-	public static int miningAndWood;
-	public static int miningAndWoodBoost; 
+	
+	public static double food;
+	public static int percentFoodBoost;
+	public static int numberFoodBoost;
+	
+	public static double miningAndWood;
+	public static int percentMiningAndWoodBoost; 
+	public static int numberMiningAndWoodBoost;
+	
+	public static double military;
+	public static int percentMilitaryBoost; 
+	public static int numberMilitaryBoost;
+	
 	public static int people;
-	public static int military;
-	public static int militaryBoost;
 	public static int reputation;	
 }
